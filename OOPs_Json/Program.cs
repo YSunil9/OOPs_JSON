0fbc204ccb62329619aad7e0f7bc5e735f8c4df3
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-16 23:07:24 +0530

    Welcome To Json Problem

---
+++ OOPs_Json/Program.cs
@@ -0,0 +1,29 @@
+﻿using OOPs_Json.Json;
+
+namespace OOPs_Json
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Inventory Management ");
+            const string InventoryDataPath = @"D:\Bridgelab\repos\OOPs_Json\OOPs_Json\Json\Inventory.json";
+            InventoryManagement inventoryData = new InventoryManagement();
+            Console.WriteLine("1.Display\n");
+            while (true)
+            {
+                Console.WriteLine("\nChoose your option: ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        inventoryData.DisplayInventory(InventoryDataPath);
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        }
+    }
+}
